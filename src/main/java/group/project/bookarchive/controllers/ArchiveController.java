90d658c6d676ed8046956cf40c36e56a73c0e02d
EiSimp <joseph.w.edu@gmail.com>
2024-06-19 21:06:00 -0700

    Changed archiveController to fit camelcase
    Changed archiveController to ArchiveController

--- src/main/java/group/project/bookarchive/controllers/ArchiveController.java
+++ src/main/java/group/project/bookarchive/controllers/ArchiveController.java
@@ -4,10 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class archiveController {
+public class ArchiveController {
     @GetMapping("/archives/view")
     public String getAllUsers(){
-         return "/homepage";
+         return "homepage";
     }
 
     @GetMapping("/login")
