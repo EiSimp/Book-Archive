50117e4ba0f635cfb459e46ed6d7abc5d94e1c0f
elllaPark <114385859+elllaPark@users.noreply.github.com>
2024-06-22 00:06:08 -0700

    modified login/signup page UI

--- src/main/java/group/project/bookarchive/controllers/ArchiveController.java
+++ src/main/java/group/project/bookarchive/controllers/ArchiveController.java
@@ -1,18 +1,38 @@
 package group.project.bookarchive.controllers;
 
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.view.RedirectView;
+
+import group.project.bookarchive.models.User;
+import group.project.bookarchive.repositories.UserRepository;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
 public class ArchiveController {
-    @GetMapping("/archives/view")
-    public String getAllUsers(){
-         return "homepage";
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping("/")
+    public RedirectView process() {
+        return new RedirectView("login");
     }
 
-    @GetMapping("/login")
-    public String login() {
-        return "login";
+    @GetMapping("/archives/view")
+    public String getAllUsers() {
+        return "homepage";
     }
 
     @GetMapping("/homepage")
@@ -21,7 +41,60 @@ public class ArchiveController {
     }
 
     @GetMapping("/signup")
-    public String signup(){
+    public String signup() {
         return "signup";
     }
+
+    @GetMapping("/login")
+    public String getLogin(Model model, HttpServletRequest request, HttpSession session) {
+        User user = (User) session.getAttribute("session_user");
+        if (user == null) {
+            return "login";
+        } else {
+            model.addAttribute("user", user);
+            return "users/protected";
+        }
+    }
+
+    @GetMapping("/forgot")
+    public String forgotpwd() {
+        return "forgotpwd";
+    }
+
+    @PostMapping("/forgot")
+    public String forgotPassword(@RequestBody String entity) {
+        // TODO: process POST request
+
+        return entity;
+    }
+
+    //
+    @PostMapping("/signup")
+    public String addUser(@RequestParam Map<String, String> newUser, HttpServletResponse response) {
+        String newName = newUser.get("username");
+        String newPw = newUser.get("password");
+        userRepository.save(new User(newPw, newName));
+        response.setStatus(HttpServletResponse.SC_CREATED);
+        return "login";
+    }
+
+    // User Login
+    @PostMapping("/login")
+    public String login(@RequestParam Map<String, String> formData, Model model, HttpServletRequest request,
+            HttpSession session) {
+        // processing login
+        String name = formData.get("name");
+        String pwd = formData.get("password");
+        List<User> userlist = userRepository.findByUsernameAndPassword(name, pwd);
+        if (userlist.isEmpty()) {
+            return "login";
+        } else {
+            // success
+            User user = userlist.get(0);
+            request.getSession().setAttribute("session_user", user);
+            model.addAttribute("user", user);
+            return "homepage";
+        }
+    }
+
 }
--- src/main/java/group/project/bookarchive/models/User.java
+++ src/main/java/group/project/bookarchive/models/User.java
@@ -5,57 +5,73 @@ import jakarta.persistence.*;
 
 //Specifies that the class is an entity mapped to a database
 @Entity
-//Specifies the name of the table in the database
+// Specifies the name of the table in the database
 @Table(name = "userinfo")
 public class User {
-    //Specifies the primary key of the entity
+    // Specifies the primary key of the entity
     @Id
-    //Specifies that the generated key is generated automatically
+    // Specifies that the generated key is generated automatically
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    //Specifies that the username has to be unique and cannot be empty
+    // Specifies that the username has to be unique and cannot be empty
     @Column(unique = true, nullable = false)
     private String username;
-    //Specifies that the password cannot be empty
+    // Specifies that the password cannot be empty
     @Column(nullable = false)
     private String password;
-    //Column for bio, added for testing
+    // Column for bio, added for testing
     @Column(length = 500)
     private String bio;
-    //Default constructor
+
+    // Default constructor
     public User() {
     }
-    //Constructor with all parameters
+
+    // Constructor with all parameters
     public User(String username, String password, String bio) {
         this.username = username;
         this.password = password;
         this.bio = bio;
     }
-    //Getters and Setters for ID
+
+    // Constructor without bio
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    // Getters and Setters for ID
     public Long getId() {
         return id;
     }
+
     public void setId(Long id) {
         this.id = id;
     }
-    //Getters and Setters for username
+
+    // Getters and Setters for username
     public String getUsername() {
         return username;
     }
+
     public void setUsername(String username) {
         this.username = username;
     }
-    //Getters and Setters for password
+
+    // Getters and Setters for password
     public String getPassword() {
         return password;
     }
+
     public void setPassword(String password) {
         this.password = password;
     }
+
     // Getters and Setters for bio
     public String getBio() {
         return bio;
     }
+
     public void setBio(String bio) {
         this.bio = bio;
     }
