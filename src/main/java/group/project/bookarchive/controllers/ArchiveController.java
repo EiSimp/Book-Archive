7f2af90bb67a2a1bcc99e47fd2a641425858c748
KarenS <karensim556@gmail.com>
2024-06-22 00:02:58 -0700

    Added a myaccount page, user img in home islink rn

--- src/main/java/group/project/bookarchive/controllers/ArchiveController.java
+++ src/main/java/group/project/bookarchive/controllers/ArchiveController.java
@@ -24,4 +24,9 @@ public class ArchiveController {
     public String signup(){
         return "signup";
     }
+
+    @GetMapping("/myaccount")
+    public String login() {
+        return "myaccount";
+    }
 }
