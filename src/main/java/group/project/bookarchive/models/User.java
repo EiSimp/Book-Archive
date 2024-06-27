ac4217c69d1c5af78c6184bebdbfa74a259e190f
EiSimp <joseph.w.edu@gmail.com>
2024-06-26 18:30:01 -0700

    Added Forced-Redirection
    1. Added a CustomAuthenticationProvider.java to handle the login logic and integrate it with spring security.
    2. Added detailed logging to ArchiveController to trace login and signup process and made sure that it did correctly handles form submission.
    3. Enabled detailed logging for spring security in application.properties.
    4. Included CSRF token in both login and signup .html's accidentally destroying the signup ui in the process.
    5. Created a security config page for redirection purposes.
    6. Provided spring security with the necessary info to make authorization decisions during authentication in User.java

--- src/main/java/group/project/bookarchive/models/User.java
+++ src/main/java/group/project/bookarchive/models/User.java
@@ -1,11 +1,16 @@
 package group.project.bookarchive.models;
 
 import jakarta.persistence.*;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
 
 @Entity
 @Table(name = "userinfo")
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -20,8 +25,7 @@ public class User {
     @Column(length = 500)
     private String bio;
 
-    public User() {
-    }
+    public User() {}
 
     public User(String username, String password, String bio) {
         this.username = username;
@@ -64,5 +68,31 @@ public class User {
 
     public void setBio(String bio) {
         this.bio = bio;
+    }
+
+    //Provides user authentication status and authorities to spring security
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singletonList(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 }
