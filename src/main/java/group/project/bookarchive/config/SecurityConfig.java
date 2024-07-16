88dcbb1f6005df699ae1d28d1081ead7b3c89ca6
William Schmidt <wcs3@sfu.ca>
2024-07-16 01:38:00 -0700

    Added ui feedback for bad logins

--- src/main/java/group/project/bookarchive/config/SecurityConfig.java
+++ src/main/java/group/project/bookarchive/config/SecurityConfig.java
@@ -1,19 +1,51 @@
 package group.project.bookarchive.config;
 
+import java.io.IOException;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.DefaultRedirectStrategy;
+import org.springframework.security.web.RedirectStrategy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import group.project.bookarchive.security.SecurityUser;
 import jakarta.servlet.DispatcherType;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+        public class LoginFailureHandler extends SimpleUrlAuthenticationFailureHandler {
+                private String urlPrefix;
+                private RedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
+
+                public LoginFailureHandler(String urlPrefix) {
+                        this.urlPrefix = urlPrefix;
+                }
+
+                @Override
+                public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
+                                AuthenticationException exception) throws IOException, ServletException {
+                        saveException(request, exception);
+                        String username = request.getParameter(
+                                        UsernamePasswordAuthenticationFilter.SPRING_SECURITY_FORM_USERNAME_KEY);
+                        String redirectUrl = urlPrefix + username;
+                        // Redirect:
+                        redirectStrategy.sendRedirect(request, response, redirectUrl);
+                }
+
+        }
+
         @Bean
         public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
                 http
@@ -39,6 +71,7 @@ public class SecurityConfig {
                                                                 response.sendRedirect("/homepage");
                                                         }
                                                 })
+                                                .failureHandler(new LoginFailureHandler("/login?error&username="))
                                                 .permitAll())
                                 .logout((logout) -> logout
                                                 .logoutSuccessUrl("/login?logoutsuccess"))
