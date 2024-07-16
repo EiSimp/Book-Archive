fd9e79275fb9bf3c490496dd13b85619f029a01a
chingyan0930 <chingyan1497@gmail.com>
2024-07-16 08:33:26 -0700

    Revert "Bookshelf manipulation, add book to bookshelf and update bookdetail"

--- src/main/java/group/project/bookarchive/repositories/BookshelfRepository.java
+++
@@ -1,8 +0,0 @@
-package group.project.bookarchive.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import group.project.bookarchive.models.Bookshelf;
-
-public interface BookshelfRepository extends JpaRepository<Bookshelf, Long> {
-    Bookshelf findByName(String name);
-}
