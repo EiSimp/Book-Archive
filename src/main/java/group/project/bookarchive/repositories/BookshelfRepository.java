7ec8b93743c96c586540dd773b9d84af97059442
chingyan0930 <chingyan1497@gmail.com>
2024-07-20 08:25:41 -0700

    Improve repository methods to fix delete function

--- src/main/java/group/project/bookarchive/repositories/BookshelfRepository.java
+++ src/main/java/group/project/bookarchive/repositories/BookshelfRepository.java
@@ -2,10 +2,13 @@ package group.project.bookarchive.repositories;
 
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 import group.project.bookarchive.models.Bookshelf;
 
 public interface BookshelfRepository extends JpaRepository<Bookshelf, Long> {
     Bookshelf findByName(String name);
+
     List<Bookshelf> findByUserId(Long userId);
+
+    Bookshelf findByNameAndUserId(String name, Long userId);
+
 }
