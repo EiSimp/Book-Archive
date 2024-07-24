a8f5b08ee6d0c55812292f9f7fe3c10b7149fb36
elllaPark <114385859+elllaPark@users.noreply.github.com>
2024-07-24 00:07:24 -0700

    my record style modification

--- src/main/resources/static/javascript/tagcloud.js
+++ src/main/resources/static/javascript/tagcloud.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
     const tags = [
-        { text: "Literature", size: "35px", top: "30%", left: "20%" },
-        { text: "Sci-Fi", size: "30px", top: "20%", left: "50%" },
+        { text: "Literature", size: "38px", top: "30%", left: "20%" },
+        { text: "Sci-Fi", size: "30px", top: "15%", left: "50%" },
         { text: "Drama", size: "25px", top: "50%", left: "70%" },
         { text: "Classic", size: "16px", top: "10%", left: "80%" },
         { text: "Culture", size: "16px", top: "60%", left: "30%" },
@@ -21,6 +21,10 @@ document.addEventListener("DOMContentLoaded", function () {
         tagElement.style.top = tag.top;
         tagElement.style.left = tag.left;
         tagElement.textContent = tag.text;
+        tagElement.style.backgroundColor = "white";
+        tagElement.style.border = "3px solid #ddd";
+        tagElement.style.borderRadius = "10px";
+        tagElement.style.padding = "0 10px";
         tagCloud.appendChild(tagElement);
     });
 });
