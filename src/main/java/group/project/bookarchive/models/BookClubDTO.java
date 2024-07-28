eafd57c85454eaf6c94ede25fc536090d1806d63
chingyan0930 <chingyan1497@gmail.com>
2024-07-28 00:24:18 -0700

    Add data model

---
+++ src/main/java/group/project/bookarchive/models/BookClubDTO.java
@@ -0,0 +1,50 @@
+package group.project.bookarchive.models;
+
+public class BookClubDTO {
+    private Long id;
+    private String name;
+    private String description;
+    private Long managerId;
+    private String managerName;
+
+    // Getters and setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Long getManagerId() {
+        return managerId;
+    }
+
+    public void setManagerId(Long managerId) {
+        this.managerId = managerId;
+    }
+
+    public String getManagerName() {
+        return managerName;
+    }
+
+    public void setManagerName(String managerName) {
+        this.managerName = managerName;
+    }
+}
